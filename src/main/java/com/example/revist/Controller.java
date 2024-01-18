bf7306f315ced53363fd2d82a2d698907dbc8e46
angeloskoroli <akoroli25@scienceleadership.org>
2024-01-18 09:12:59 -0500

    hvhj

--- src/main/java/com/example/revist/Controller.java
+++ src/main/java/com/example/revist/Controller.java
@@ -2,6 +2,8 @@ package com.example.revist;
 
 import javafx.scene.control.TableView;
 
+import java.io.*;
+
 public class Controller {
     public TableView NBATV;
     public TableView OlympiaTV;
@@ -16,6 +18,26 @@ public void initialize() throws Exception {
         NBATV.getItems().add(sports);
     }
 
-
-}
-    }
+    //void saveData() throws Exception {
+        //File fileForData = new File("Angelos");
+        //FileOutputStream outputStream = new FileOutputStream(fileForData);
+        //ObjectOutputStream objOutputStream= new ObjectOutputStream(outputStream);
+        //objOutputStream.writeObject(AnimeTierlist.getItems().size());
+        //for (AnimeTier eachData : AnimeTierlist.getItems()) {
+            //objOutputStream.writeObject(eachData);
+        }
+        //objOutputStream.flush();
+    //}
+    //void restoreData() throws Exception {
+        //File fileForData = new File("Angelos");
+        //FileInputStream inputStream = new FileInputStream(fileForData);
+        //ObjectInputStream objInputStream = new ObjectInputStream(inputStream);
+        //Integer numOfSavedObjects = (Integer)objInputStream.readObject();
+        //for (int i = 0; i < numOfSavedObjects; i = i + 1) {
+            //AnimeTier eachData = (AnimeTier) objInputStream.readObject();
+            //AnimeTierlist.getItems().add(eachData);
+        }
+        //inputStream.close();
+    //}
+//}
+    //}
